42b86a8b4d5b2c3d62291459d7e34b5151088103
Anindya Diany <anindya@Anindyas-MacBook-Pro-3.local>
2025-09-03 22:57:34 +0700

    Initial commit for Laravel project

---
+++ resources/views/anin/index.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>anin network | home</title>
+</head>
+<body>
+    <h2>available</h2>
+    <p>{{ $greeting }}</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome to anins web</h1>
+    <p>click this button to view the list! </p>
+
+    <a href="/anin" class="button">
+        find ninjas
+    </a>
+</body>
+</html>
